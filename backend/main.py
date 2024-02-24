dbc6393dd9b04f05534279701b86a01a32e20d95
Margot M. Baisch <margot.mb@outlook.com>
2024-02-23 21:58:25 -0300

    MainApi Update

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import RedirectResponse
 from dbcrud import DBCrud
 from models.data import Data
 import sqlite3
@@ -10,14 +10,27 @@ con = DBCrud()
 #Generate Tables
 con.create_table()
 
-data = Data(user_url = "url_test", url_identifier = "identificador_teste")
+data = Data(user_url = "https://google.com", url_identifier = "abcde")
 con.create_data(data)
 
 #FastAPI -> 'uvicorn main:app --reload' to run server
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return "Homepage"
+
 @app.get("/{url_id}/")
 def read(url_id):
     #returns tuple
     query = con.read_url(url_id)
-    return query[0]
+    
+    if query:
+        return RedirectResponse(query[0])
+    else:
+        return None
+
+
+@app.post("/create_url_id/{user_url}")
+def create_url_id(user_url):
+    return ""
