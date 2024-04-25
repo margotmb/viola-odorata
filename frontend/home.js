c961bb843d3b1a858a713b34efe8ec5afdec9fc4
Margot <margot.mb@outlook.com>
2024-04-25 11:20:57 -0300

    front fetch

--- frontend/home.js
+++ frontend/home.js
@@ -1,19 +1,22 @@
 function Shortener() {
-  const api_url = "" + "/create_url_id/";
   let url_field = document.getElementById("url").value;
+  console.log(url_field)
+  const shortener_url = "http://127.0.0.1:8000/"
+  // header content-type is required
   try {
     const url = new URL(url_field);
-    fetch(api_url, {
+    console.log(url.href)
+    fetch("http://127.0.0.1:8000/create_url_id", {
       method: "POST",
       mode: "cors",
-      body: JSON.stringify({ user_url: url.href }),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({user_url: url.href}),
     })
-      .then((Response) => {
-        return Response.json();
-      })
+      .then((data) => data.json())
       .then((data) => {
-        console.log(data);
+        window.confirm("Your short url is: " + shortener_url + data.url_id)
       });
   } catch (e) {
     alert("Invalid URL");
