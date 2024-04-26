53ee9b80505317d6783a9cb0961526e28c1f6f54
Margot <margot.mb@outlook.com>
2024-04-26 11:03:33 -0300

    Updating Frontend to React w/ Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import UrlShortener from "./urlshortener";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UrlShortener/>} />
+      </Routes>    
+    </BrowserRouter> 
+  );
+}
+
+export default App
---
+++ frontend/src/urlshortener.jsx
@@ -0,0 +1,57 @@
+import React, {useRef} from "react";
+import './urlshortener.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function UrlShortener(){
+    const url_field = useRef(null)
+    console.log(url_field.current)
+
+    const handleSubmit = () => {
+
+        const shortener_url = ""
+        // header content-type is required
+        try {
+            const url = new URL(url_field.current);
+            console.log(url.href)
+            fetch("/create_url_id", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({user_url: url.href}),
+            })
+            .then((data) => data.json())
+            .then((data) => {
+                window.confirm("Your short url is: " + shortener_url + data.url_id)
+            }).catch((e) => {
+                alert(e);
+            });
+        } catch (e) {
+            alert("Invalid URL");
+        }
+    }
+    return (
+        <React.Fragment>
+        <div className="Auth-form-container">
+            <form className="Auth-form" onSubmit={handleSubmit}>
+                <h3 className="Auth-form-title">Url Shortener</h3>
+                <div className="Auth-form-content">
+                    <input 
+                        id="url" 
+                        type="text" 
+                        name="url" 
+                        required
+                        onChange={(e) => (url_field.current = e.target.value)}></input>
+                    <button 
+                        type="submit"
+                        onClick={() => {handleSubmit}}>
+                            OK
+                    </button>
+                </div>
+            </form>
+        </div>
+        </React.Fragment>
+    )
+}
+export default Login
